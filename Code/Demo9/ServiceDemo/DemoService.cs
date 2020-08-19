16372a104bece56af30f8a12a6e90bf53d7c82dc
Abdul-Rahman Abdurrohman <abdulbarakeh98@gmail.com>
2020-08-19 13:00:15 +0200

    init

---
+++ Code/Demo9/ServiceDemo/DemoService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDemo
+{
+    public partial class DemoService : ServiceBase
+    {
+        public DemoService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.AppendAllText("C:\\Users\abdul\\Desktop\\KPUEksamen\\Code\\Demo9\\logKPUservice.txt",
+               "\n Service has been started" + " @" + DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            System.IO.File.AppendAllText("C:\\Users\abdul\\Desktop\\KPUEksamen\\Code\\Demo9\\logKPUservice.txt",
+               "\n Service has been stopped" + " @" + DateTime.Now);
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.IO.File.AppendAllText("C:\\Users\abdul\\Desktop\\KPUEksamen\\Code\\Demo9\\logKPUservice.txt",
+              "\n Change happened in " + e.FullPath + " @" + DateTime.Now + "\n This happened: " + e.ChangeType);
+
+        }
+        private void fileSystemWatcher1_Renamed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.IO.File.AppendAllText("C:\\Users\abdul\\Desktop\\KPUEksamen\\Code\\Demo9\\logKPUservice.txt",
+                "\n Change happened in " + e.FullPath + " @" + DateTime.Now + "\n This happened: " + e.ChangeType);
+
+        }
+        private void fileSystemWatcher1_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.IO.File.AppendAllText("C:\\Users\abdul\\Desktop\\KPUEksamen\\Code\\Demo9\\logKPUservice.txt",
+                "\n Change happened in " + e.FullPath + " @" + DateTime.Now + "\n This happened: " + e.ChangeType);
+
+        }
+        private void fileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.IO.File.AppendAllText("C:\\Users\abdul\\Desktop\\KPUEksamen\\Code\\Demo9\\logKPUservice.txt",
+                "\n Change happened in " + e.FullPath + " @" + DateTime.Now + "\n This happened: " + e.ChangeType);
+
+        }
+    }
+}
